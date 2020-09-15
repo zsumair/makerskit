41a52a0449d4013c54c59b4b35b575029cb559e5
syedzoheb1242@gmail.com <syedzoheb1242@gmail.com>
2020-09-15 21:44:19 +0530

    analytics change

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,19 +25,11 @@ module.exports = {
         // The property ID; the tracking code won't be generated without it
         trackingId: process.env.GA_TRACKING_ID,
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
+        head: true,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
         respectDNT: false,
-        // Avoids sending pageview hits from custom paths
-        // exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        defer: false,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
         cookieDomain: "https://www.makerskit.tech/",
       },
     },
