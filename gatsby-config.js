566f688708c83a5e29ab72f372e50de6f0f42a7c
syedzoheb1242@gmail.com <syedzoheb1242@gmail.com>
2020-09-15 21:36:55 +0530

    updated google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-178028363-1",
+        trackingId: process.env.GA_TRACKING_ID,
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: false,
         // Setting this parameter is optional
